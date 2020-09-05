4e1ed95d4cbced10d1c71755d1fe63ba9e26b02e
Niharika-Vankireddy <70135441+Niharika-Vankireddy@users.noreply.github.com>
2020-09-05 23:39:25 +0530

    Added script.html

--- script.js
+++ script.js
@@ -131,3 +131,29 @@ return 'not palindrome false';
 }
 }
 
+function sortStrChars(str) {
+    if (!str) {
+        return;
+    }
+    str = str.split('');
+    str = str.sort();
+    str = str.join('');
+    return str;
+}
+
+function getGroupedAnagrams(words){
+    const anagrams = {}; // {abc:[abc,cba], dell:[dell, ledl]}
+    words.forEach((word)=>{
+        const sortedWord = sortStrChars(word);
+        if (anagrams[sortedWord]) {
+            return anagrams[sortedWord].push(word);
+        }
+        anagrams[sortedWord] = [word];
+    });
+    return anagrams;
+}
+
+const groupedAnagrams = getGroupedAnagrams(words);
+for(const sortedWord in groupedAnagrams){
+    console.log(groupedAnagrams[sortedWord].toString());
+}
